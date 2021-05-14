61a4fb219ee13d4ba52a35c40029c402abd94091
gshukov98 <56069656+gshukov98@users.noreply.github.com>
2021-05-14 13:20:46 +0300

    Initial commit

---
+++ src/Localizations.PhraseApp/Internal/PhraseAppSourceLocaleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Localizations.PhraseApp.Internal
+{
+    internal class PhraseAppSourceLocaleModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
