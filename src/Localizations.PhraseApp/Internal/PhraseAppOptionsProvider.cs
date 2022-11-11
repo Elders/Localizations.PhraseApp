9a3947469aac14139150a8a835678cad364b474d
sert2215 <sertstepka@gmail.com>
2022-11-11 11:49:38 +0200

    Add constants for default values

--- src/Localizations.PhraseApp/Internal/PhraseAppOptionsProvider.cs
+++ src/Localizations.PhraseApp/Internal/PhraseAppOptionsProvider.cs
@@ -5,18 +5,23 @@ namespace Localizations.PhraseApp.Internal
 {
     internal class PhraseAppOptionsProvider : OptionsProviderBase<PhraseAppOptions>
     {
+        private const string DefaultAddress = "https://api.phraseapp.com/api/v2/";
+        private const string DefaultLocale = "en";
+        private const string DoNotUseStrictLocale = "false";
+        private const string DefaultTtl = "5";
+
         public const string Section = "localization:phraseapp";
 
         public PhraseAppOptionsProvider(IConfiguration configuration) : base(configuration) { }
 
         public override void Configure(PhraseAppOptions options)
         {
-            options.Address = configuration[$"{Section}:address"] ?? "https://api.phraseapp.com/api/v2/";
+            options.Address = configuration[$"{Section}:address"] ?? DefaultAddress;
             options.AccessToken = configuration[$"{Section}:accesstoken"];
             options.ProjectId = configuration[$"{Section}:projectid"];
-            options.DefaultLocale = configuration[$"{Section}:defaultlocale"] ?? "en";
-            options.UseStrictLocale = bool.Parse(configuration[$"{Section}:usestrictlocale"] ?? "false");
-            options.TtlInMinutes = int.Parse(configuration[$"{Section}:ttlinminutes"] ?? "5");
+            options.DefaultLocale = configuration[$"{Section}:defaultlocale"] ?? DefaultLocale;
+            options.UseStrictLocale = bool.Parse(configuration[$"{Section}:usestrictlocale"] ?? DoNotUseStrictLocale);
+            options.TtlInMinutes = int.Parse(configuration[$"{Section}:ttlinminutes"] ?? DefaultTtl);
         }
     }
 }
